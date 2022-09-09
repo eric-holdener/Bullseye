d11d4bf43a1b0f6b2d4d9e0f01850ab0d84088e2
eric <eholdener12@gmail.com>
2022-09-09 09:27:05 -0700

    finished part 2 of ray wenderlich iOS app tutorial

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -10,20 +10,54 @@ import SwiftUI
 struct ContentView: View {
     
     @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
     
     var body: some View {
         VStack {
-            Text("Welcome to my first app!")
-                .fontWeight(.semibold)
-                .foregroundColor(Color.green)
+            Spacer()
+            // Target Row
+            HStack {
+                Text("Put the bullseye as close as you can to:")
+                Text("100")
+            }
+            .padding()
+            
+            Spacer()
+            
+            // Slider Row
+            HStack {
+                Text("1")
+                Slider(value: self.$sliderValue, in: 1...100)
+                Text("100")
+            }
                 .padding()
+            
+            // Button Row
             Button("Hit Me!") {
                 self.alertIsVisible = true
             }
             .alert(isPresented: $alertIsVisible) {
                 () -> Alert in
-                return Alert(title: Text("Hello there!"), message: Text("This is my first pop-up!"), dismissButton: .default(Text("Awesome")))
+                return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(self.sliderValue)"), dismissButton: .default(Text("Awesome")))
             }
+            
+            Spacer()
+            
+            //Score Row
+            HStack() {
+                Button("Start Over") {
+                    
+                }
+                Spacer()
+                Text("Score: 999999")
+                Spacer()
+                Text("Round: 999")
+                Spacer()
+                Button("Info") {
+                    
+                }
+            }
+            .padding()
         }
     }
 }
@@ -31,5 +65,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13 Pro Max")
+            .previewInterfaceOrientation(.landscapeLeft)
     }
 }
