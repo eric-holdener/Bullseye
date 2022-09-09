d4679d107aeb4e2b2ba5cbd2011c7a29ae42eeba
eric <eholdener12@gmail.com>
2022-09-08 18:29:00 -0700

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Eric Holdener on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
