6cc2edea7eff2b97c0f3c31067addb8bff7395cd
eric <eholdener12@gmail.com>
2022-09-10 09:44:45 -0700

    finished basic app

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,8 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var alertIsVisible: Bool = false
-    @State var sliderValue: Double = 50.0
+    @State var alertIsVisible = false
+    @State var infoIsVisible = false
+    @State var sliderValue = 50.0
+    @State var target = Int.random(in: 1...100)
+    @State var score = 0
+    @State var round = 1
+    
+    func pointsForCurrentRound() -> Int {
+        return 100 - abs(target - sliderValueRounded())
+    }
+    
+    func sliderValueRounded() -> Int {
+        return Int(sliderValue.rounded())
+    }
+    
+    func getRandomTarget() -> Int {
+        Int.random(in: 1...100)
+    }
+    
+    func getAlertTitle() -> Text {
+        let points = pointsForCurrentRound()
+        if (points == 100) {
+            return Text("Perfect!")
+        } else if (points > 85) {
+            return Text("Almost!")
+        } else {
+            return Text("Not quite. Try again!")
+        }
+    }
+    
+    func getScore() -> Int {
+        let roundScore = pointsForCurrentRound()
+        if (roundScore == 100) {
+            return roundScore + 100
+        } else if (roundScore == 99) {
+            return roundScore + 50
+        } else {
+            return roundScore
+        }
+    }
+    
+    func newGame() {
+        self.score = 0
+        self.round = 1
+        self.sliderValue = 50.0
+        self.target = getRandomTarget()
+    }
     
     var body: some View {
         VStack {
@@ -18,7 +63,7 @@ struct ContentView: View {
             // Target Row
             HStack {
                 Text("Put the bullseye as close as you can to:")
-                Text("100")
+                Text("\(target)")
             }
             .padding()
             
@@ -27,7 +72,7 @@ struct ContentView: View {
             // Slider Row
             HStack {
                 Text("1")
-                Slider(value: self.$sliderValue, in: 1...100)
+                Slider(value: $sliderValue, in: 1...100)
                 Text("100")
             }
                 .padding()
@@ -38,7 +83,11 @@ struct ContentView: View {
             }
             .alert(isPresented: $alertIsVisible) {
                 () -> Alert in
-                return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(self.sliderValue)"), dismissButton: .default(Text("Awesome")))
+                return Alert(title: getAlertTitle(), message: Text("The slider's value is \(Int(sliderValueRounded()))\n" + "You scored \(getScore()) points this round."), dismissButton: .default(Text("Awesome"), action: {
+                    self.score += getScore()
+                    self.round += 1
+                    self.target = getRandomTarget()
+                }))
             }
             
             Spacer()
@@ -46,15 +95,19 @@ struct ContentView: View {
             //Score Row
             HStack() {
                 Button("Start Over") {
-                    
+                    newGame()
                 }
                 Spacer()
-                Text("Score: 999999")
+                Text("Score: \(score)")
                 Spacer()
-                Text("Round: 999")
+                Text("Round: \(round)")
                 Spacer()
                 Button("Info") {
-                    
+                    self.infoIsVisible = true
+                }
+                .alert(isPresented: $infoIsVisible) {
+                    () -> Alert in
+                    return Alert(title: Text("Bullseye"), message: Text("Copyright by Eric Holdener, 2022"), dismissButton: .default(Text("Dismiss")))
                 }
             }
             .padding()
