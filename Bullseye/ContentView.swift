0c97ab710cf282162c5731c9f6811bdeadd93390
eric <eholdener12@gmail.com>
2022-09-11 09:48:38 -0700

    finished bullseye

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -10,11 +10,52 @@ import SwiftUI
 struct ContentView: View {
     
     @State var alertIsVisible = false
-    @State var infoIsVisible = false
     @State var sliderValue = 50.0
     @State var target = Int.random(in: 1...100)
     @State var score = 0
     @State var round = 1
+    let midnightBlue = Color(red: 0.0 / 255.0, green: 51.0 / 255.0, blue: 102.0 / 255.0)
+    
+    struct LabelStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .foregroundColor(Color.white)
+                .font(.custom("Arial Rounded MT Bold", size: 18))
+                .modifier(DropShadow())
+        }
+    }
+    
+    struct ValueStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .foregroundColor(Color.yellow)
+                .font(.custom("Arial Rounded MT Bold", size: 24))
+                .modifier(DropShadow())
+        }
+    }
+    
+    struct DropShadow: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .shadow(color: .black, radius: 5, x: 2, y: 2)
+        }
+    }
+    
+    struct ButtonLargeTextStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .foregroundColor(.black)
+                .font(.custom("Arial Rounded MT Bold", size: 18))
+        }
+    }
+    
+    struct ButtonSmallTextStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .foregroundColor(.black)
+                .font(.custom("Arial Rounded MT Bold", size: 12))
+        }
+    }
     
     func pointsForCurrentRound() -> Int {
         return 100 - abs(target - sliderValueRounded())
@@ -58,67 +99,87 @@ struct ContentView: View {
     }
     
     var body: some View {
-        VStack {
-            Spacer()
-            // Target Row
-            HStack {
-                Text("Put the bullseye as close as you can to:")
-                Text("\(target)")
-            }
-            .padding()
-            
-            Spacer()
-            
-            // Slider Row
-            HStack {
-                Text("1")
-                Slider(value: $sliderValue, in: 1...100)
-                Text("100")
-            }
+        NavigationView {
+            VStack() {
+                Spacer()
+                // Target Row
+                HStack {
+                    Text("Put the bullseye as close as you can to:").modifier(LabelStyle())
+                    Text("\(target)").modifier(ValueStyle())
+                }
                 .padding()
-            
-            // Button Row
-            Button("Hit Me!") {
-                self.alertIsVisible = true
-            }
-            .alert(isPresented: $alertIsVisible) {
-                () -> Alert in
-                return Alert(title: getAlertTitle(), message: Text("The slider's value is \(Int(sliderValueRounded()))\n" + "You scored \(getScore()) points this round."), dismissButton: .default(Text("Awesome"), action: {
-                    self.score += getScore()
-                    self.round += 1
-                    self.target = getRandomTarget()
-                }))
-            }
-            
-            Spacer()
-            
-            //Score Row
-            HStack() {
-                Button("Start Over") {
-                    newGame()
+                
+                Spacer()
+                
+                // Slider Row
+                HStack {
+                    Text("1").modifier(LabelStyle())
+                    Slider(value: $sliderValue, in: 1...100)
+                    Text("100").modifier(LabelStyle())
                 }
-                Spacer()
-                Text("Score: \(score)")
-                Spacer()
-                Text("Round: \(round)")
-                Spacer()
-                Button("Info") {
-                    self.infoIsVisible = true
+                .padding()
+                
+                // Button Row
+                Button("Hit Me!") {
+                    self.alertIsVisible = true
                 }
-                .alert(isPresented: $infoIsVisible) {
+                .modifier(ButtonLargeTextStyle())
+                .background(Image("Button"))
+                .modifier(DropShadow())
+                .alert(isPresented: $alertIsVisible) {
                     () -> Alert in
-                    return Alert(title: Text("Bullseye"), message: Text("Copyright by Eric Holdener, 2022"), dismissButton: .default(Text("Dismiss")))
+                    return Alert(title: getAlertTitle(), message: Text("The slider's value is \(Int(sliderValueRounded()))\n" + "You scored \(getScore()) points this round."), dismissButton: .default(Text("Awesome"), action: {
+                        self.score += getScore()
+                        self.round += 1
+                        self.target = getRandomTarget()
+                    }))
                 }
+                
+                Spacer()
+                
+                //Score Row
+                HStack() {
+                    Button {
+                        newGame()
+                    } label: {
+                        HStack {
+                            Image("StartOverIcon")
+                            Text("Start Over")
+                        }
+                    }
+                    .background(Image("Button"))
+                    .modifier(DropShadow())
+                    .modifier(ButtonSmallTextStyle())
+                    Spacer()
+                    Text("Score:").modifier(LabelStyle())
+                    Text("\(score)").modifier(ValueStyle())
+                    Spacer()
+                    Text("Round:").modifier(LabelStyle())
+                    Text("\(round)").modifier(ValueStyle())
+                    Spacer()
+                    NavigationLink(destination: AboutView()) {
+                        HStack {
+                            Image("InfoIcon")
+                            Text("Info")
+                        }
+                    }
+                    .modifier(ButtonSmallTextStyle())
+                    .background(Image("Button"))
+                    .modifier(DropShadow())
+                }
+                .padding()
             }
-            .padding()
+            .background(Image("Background"), alignment: .center)
+            .accentColor(midnightBlue)
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewDevice("iPhone 13 Pro Max")
+            .previewDevice("iPhone 12")
             .previewInterfaceOrientation(.landscapeLeft)
     }
 }
